bb5f332202613019bae03c96a7974a8ac30ae916
Luciene <lucienedelima8@gmail.com>
2024-06-08 19:45:43 -0300

    comentando o arquivo nome_ator js

--- nome_ator.js
+++ nome_ator.js
@@ -1,9 +1,13 @@
 /*Seção Atores: JS pra aparecer o nome do ator ao clicar na foto*/
 
+
+/*Variável pra armazenar img_ator que é class da imagem de cada ator*/
 let foto_ator = document.getElementsByClassName('img_ator');
 
-
+/*Função da img do ator no html: onclick mostrar_nome */
 function mostrar_nome() {
+
+    /*Id click_nome do parágrafo com nome do ator*/
     document.getElementById('click_nome').style.display = 'block';
     document.getElementById('click_nome1').style.display = 'block';
 
