ed535fe8b2a2088f74f3f18326ede2c3104e87b2
Luciene <lucienedelima8@gmail.com>
2024-06-10 20:01:06 -0300

    criei um botão pra aparecer o nome dos atores, mudei a função por isso

--- nome_ator.js
+++ nome_ator.js
@@ -2,21 +2,31 @@
 
 
 /*Variável pra armazenar img_ator que é class da imagem de cada ator*/
-let foto_ator = document.getElementsByClassName('img_ator');
+/*let foto_ator = document.getElementsByClassName('img_ator');
 
 /*Função da img do ator no html: onclick mostrar_nome */
-function mostrar_nome() {
+/*function mostrar_nome() {
 
     /*Id click_nome do parágrafo com nome do ator*/
-    document.getElementById('click_nome').style.display = 'block';
+    /*document.getElementById('click_nome').style.display = 'block';
     document.getElementById('click_nome1').style.display = 'block';
 
 
     /*document.getElementsByClassName('click_nome').style.display = 'block';*/
 
+/*}*/
+
+
+
+/*Variável pra armazenar o botão clicável*/
+let nome_ator = document.getElementsByClassName('btn_nome_atores');
+
+/*Função do botão no html: onclick mostrar_nome */
+function mostrar_nome() {
+    
+    document.getElementById('click_nome').style.display = 'block';
+    document.getElementById('click_nome1').style.display = 'block';
 }
-
-
 
 
 
