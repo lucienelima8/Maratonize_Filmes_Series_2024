dc033c2f86e2a618b3f4bafdd020e07e8d6b64a9
Luciene <lucienedelima8@gmail.com>
2024-06-11 19:17:09 -0300

    ajustando o js de atores, colocando um for pra pegar todas as classes dos parágrafos

--- nome_ator.js
+++ nome_ator.js
@@ -1,47 +1,22 @@
-/*Seção Atores: JS pra aparecer o nome do ator ao clicar na foto*/
-
-
-/*Variável pra armazenar img_ator que é class da imagem de cada ator*/
-/*let foto_ator = document.getElementsByClassName('img_ator');
-
-/*Função da img do ator no html: onclick mostrar_nome */
-/*function mostrar_nome() {
-
-    /*Id click_nome do parágrafo com nome do ator*/
-    /*document.getElementById('click_nome').style.display = 'block';
-    document.getElementById('click_nome1').style.display = 'block';
-
-
-    /*document.getElementsByClassName('click_nome').style.display = 'block';*/
-
-/*}*/
-
+/*Seção Atores: JS pra aparecer o nome do ator ao clicar no botão*/
 
 
 /*Variável pra armazenar o botão clicável*/
-let nome_ator = document.getElementsByClassName('btn_nome_atores');
+let nome_ator = document.getElementById('btn_nome_atores');
+
+var texto = '';
 
 /*Função do botão no html: onclick mostrar_nome */
 function mostrar_nome() {
     
-    document.getElementById('click_nome').style.display = 'block';
+    var paragrafos = document.getElementsByClassName("click_nome");
+
+    /*for para pegar todos os índices das classes*/
+    for (var i = 0; i < paragrafos.length; i++) {
+
+       texto += paragrafos [i].style.display = 'block';
+        
+    }
     
-    document.getElementById('click_nome1').style.display = 'block';
-
-    document.getElementById('click_nome2').style.display = 'block';
-
-    document.getElementById('click_nome3').style.display = 'block';
-
 }
-
-
-
-
-
-
-
-
-
-
-
 
